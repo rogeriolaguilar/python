1d0253dfe7c51c795992edd76406a319ac3f72a7
Rogério Libarino Aguilar <rogerio.l.aguilar@gmail.com>
2018-08-27 13:59:46 -0300

    install/use pygal module to render graphics

--- lessons/simple_pygal.py
+++ lessons/simple_pygal.py
@@ -3,11 +3,17 @@ Simple example code for pygal, use to test installation
 From http://www.pygal.org/en/latest/index.html
 """
 
+# For install packages leading with several installed python versions use:
+# python -m pip install [module] [module2]...
+# Ex:
+# python -m pip install pygal lxml
+# or
+# python3.6 -m pip install pygal lxml
 
 import pygal
 
 # Saves to SVG file - http://www.pygal.org/en/stable/documentation/output.html#file
-pygal.Bar()(1, 3, 3, 7)(1, 6, 6, 4).render_to_file("pygal_test.svg")
+#pygal.Bar()(1, 3, 3, 7)(1, 6, 6, 4).render_to_file("pygal_test.svg")
 
 # Render in browser - needs packages lxml installed - http://www.pygal.org/en/stable/documentation/output.html#browser
-#pygal.Bar()(1, 3, 3, 7)(1, 6, 6, 4).render_in_browser()
+pygal.Bar()(1, 3, 3, 7)(1, 6, 6, 4).render_in_browser()
